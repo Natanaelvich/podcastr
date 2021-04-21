ce63fe1c10cdf56582b8669e2939d0214203ee65
Natanael Lima <taelima1997@gmail.com>
2021-04-20 23:07:50 -0300

    add initial home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <h1>Hello word</h1>;
-}
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+
+type HomeProps = {
+  episodes: any;
+};
+
+export const Home = ({ episodes }: HomeProps) => {
+  return (
+    <>
+      <Head>
+        <title>Home | Podcastr</title>
+      </Head>
+      <div>
+        <h1>Index</h1>
+        <p>{JSON.stringify(episodes)}</p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+};
