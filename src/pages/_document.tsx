7fe351e580adbfefe315080ede1e5f1e7c114283
Natanael Lima <taelima1997@gmail.com>
2021-04-20 23:07:35 -0300

    add favicon and update font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,11 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="shortcut icon" href="/favicon.png" />
+
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
             rel="stylesheet"
           />
         </Head>
